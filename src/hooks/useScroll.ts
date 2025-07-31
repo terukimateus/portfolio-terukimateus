831552e70a202324f31d7c2ddbffe8fd6840bcfc
Mateus Matubara <mateus.matubara@db1.com.br>
2025-07-31 10:33:02 -0300

    feat: add useScroll hook

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,16 @@
+import { useAnimate, useInView } from "motion/react";
+import { useEffect, useRef } from "react";
+
+export const useScrollAnimation = () => {
+  const [, animate] = useAnimate();
+  const ref = useRef<HTMLElement | null>(null);
+  const inView = useInView(ref);
+
+  useEffect(() => {
+    if (inView && ref.current) {
+      animate(ref.current, "visible");
+    }
+  }, [animate, inView]);
+
+  return { ref, animate };
+};
